b14973b04c3d42c1b88c431007cf567429722651
Kanhaiya Verma <57759817+kanhaiya8608@users.noreply.github.com>
2023-12-06 09:45:03 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src/"),
-      components: `${path.resolve(__dirname, "./src/components/")}`,
-      public: `${path.resolve(__dirname, "./public/")}`,
-      pages: path.resolve(__dirname, "../pages"),
-    },
-  },
-});
+  
+})
