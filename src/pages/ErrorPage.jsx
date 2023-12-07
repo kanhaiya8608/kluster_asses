407441d8afa30c708a56ce23f55f86a10df92434
Kanhaiya Verma <57759817+kanhaiya8608@users.noreply.github.com>
2023-12-08 05:05:43 +0530

    Update ErrorPage.jsx

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,6 +1,12 @@
 import Error1 from '../assets/404.png';
-
+import { useNavigate } from 'react-router-dom';
 function ErrorPage() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    // Navigate to the shop page
+    navigate('/');
+  }
   return (
     <div className='bg-white  flex flex-col items-center justify-center py-20'>
       <img src={Error1} alt="Error 404" className='mb-4' />
@@ -13,7 +19,7 @@ function ErrorPage() {
         Sorry, We can’t find the page you’re looking for.
       </p>
 
-      <button className='bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600'>
+      <button onClick={handleClick} className='bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600'>
         Go Back
       </button>
     </div>
